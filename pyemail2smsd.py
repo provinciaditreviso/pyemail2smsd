d0197889490b98983ec7a2e68e46274b0f91eff9
Luca Lorenzetto <lorenzetto.luca@gmail.com>
2013-01-30 18:17:09 +0100

    A fix for an anomalous crash on production

--- pyemail2smsd.py
+++ pyemail2smsd.py
@@ -1,22 +1,45 @@
 #!/usr/bin/env python
+""" 
+pyemail2smsd.py - A python pipe from postfix to smstools
+Copyright (C) 2013 Luca 'remix_tj' Lorenzetto <lorenzettoluca@provincia.treviso.it>
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+
+"""
 import email
 import sys
 import tempfile
 import os
 
+
+# Set the path of the outgoing folder for smstools
 OUTQUEUEDIR = "/var/spool/sms/outgoing/"
 
-
+# Keeps the stdin, since is used as pipe on postfix
 mail = email.message_from_string(sys.stdin.read())
 sms = ""
 sms += "To: " + mail["X-Original-To"] + "\n"
 
 body = "\n"
 
-
+# Only plain text part is allowed, else the sms will be blank.
 for part in mail.walk():
 	if part.get_content_type() == 'text/plain':
 		content = part.get_payload()
+		# at the first empty line, stops building the message
 		for row in content.splitlines():
 			if len(row) > 0:
 				body += row+"\n"
@@ -25,10 +48,12 @@ for part in mail.walk():
 	else:
 		body = ""
 
-body += "-- "+mail["From"]+ "\n"
+# adds the sender email as signature
+body += "--\n"+str(mail["From"])+ "\n"
 
 sms += body
 
+# Write the sms to the OUTQUEUEDIR
 fdsms, fsms = tempfile.mkstemp(prefix="sms-",dir=OUTQUEUEDIR)
 outf=os.fdopen(fdsms,'wt')
 outf.write(sms)

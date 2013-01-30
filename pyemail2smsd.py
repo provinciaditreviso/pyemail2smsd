ac4591da3132eaf8bd8e5f86fda726d516141037
Luca Lorenzetto <lorenzetto.luca@gmail.com>
2013-01-30 17:06:45 +0100

    Working version

--- pyemail2smsd.py
+++ pyemail2smsd.py
@@ -1,2 +1,35 @@
 #!/usr/bin/env python
+import email
+import sys
+import tempfile
+import os
 
+OUTQUEUEDIR = "/var/spool/sms/outgoing/"
+
+
+mail = email.message_from_string(sys.stdin.read())
+sms = ""
+sms += "To: " + mail["X-Original-To"] + "\n"
+
+body = "\n"
+
+
+for part in mail.walk():
+	if part.get_content_type() == 'text/plain':
+		content = part.get_payload()
+		for row in content.splitlines():
+			if len(row) > 0:
+				body += row+"\n"
+			else:
+				break
+	else:
+		body = ""
+
+body += "-- "+mail["From"]+ "\n"
+
+sms += body
+
+fdsms, fsms = tempfile.mkstemp(prefix="sms-",dir=OUTQUEUEDIR)
+outf=os.fdopen(fdsms,'wt')
+outf.write(sms)
+outf.close()
